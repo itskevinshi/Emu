f848f26ed37d5493003c9bfe668b4651beceabb6
itskevinshi <kevinshi0@outlook.com>
2023-06-29 21:26:27 -0400

    Add cases for dissassembler

--- disassembler.c
+++ disassembler.c
@@ -21,7 +21,58 @@ int Disassemble8080Op(unsigned char *codebuffer, int pc)
         case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;    
         case 0x07: printf("RLC"); break;    
         case 0x08: printf("NOP"); break;    
-        /* ........ */    
+        case 0x09: printf("DAD    B"); break;
+        case 0x0a: printf("LDAX   B"); break;
+        case 0x0b: printf("DCX    B"); break;
+        case 0x0c: printf("INR    C"); break;
+        case 0x0d: printf("DCR    C"); break;
+        case 0x0e: printf("MVI    C,#$%02x", code[1]); opbytes = 2;	break;
+        case 0x0f: printf("RRC"); break;
+        case 0x10:
+        case 0x11:
+        case 0x12:
+        case 0x13:
+        case 0x14:
+        case 0x15:
+        case 0x16:
+        case 0x17:
+        case 0x18:
+        case 0x19:
+        case 0x1a:
+        case 0x1b:
+        case 0x1c:
+        case 0x1d:
+        case 0x1e:
+        case 0x1f:
+        case 0x20:
+        case 0x21:
+        case 0x22:
+        case 0x23:
+        case 0x24:
+        case 0x25:
+        case 0x26:
+        case 0x27:
+        case 0x28:
+        case 0x29:
+        case 0x2a:
+        case 0x2b:
+        case 0x2c:
+        case 0x2d:
+        case 0x2e:
+        case 0x2f:
+        case 0x31:
+        case 0x32:
+        case 0x33:
+        case 0x34:
+        case 0x35:
+        case 0x36:
+        case 0x37:
+        case 0x38:
+        case 0x39:
+        case 0x3a:
+        case 0x3b:
+        case 0x3c:
+        case 0x3d:
         case 0x3e: printf("MVI    A,#0x%02x", code[1]); opbytes = 2; break;    
         /* ........ */    
         case 0xc3: printf("JMP    $%02x%02x",code[2],code[1]); opbytes = 3; break;    
