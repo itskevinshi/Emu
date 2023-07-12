cfb087b2499083c9480bb06b8e92ee6f92c1a1dd
itskevinshi <kevinshi0@outlook.com>
2023-07-11 21:11:02 -0400

    Add cases for 0x60-0x7f

--- disassembler.c
+++ disassembler.c
@@ -131,39 +131,39 @@ int Disassemble8080Op(unsigned char *codebuffer, int pc)
         case 0x5e: printf("MOV    E,M"); break;
         case 0x5f: printf("MOV    E,A"); break;
 
-        case 0x60:
-        case 0x61:
-        case 0x62:
-        case 0x63:
-        case 0x64:
-        case 0x65:
-        case 0x66:
-        case 0x67:
-        case 0x68:
-        case 0x69:
-        case 0x6a:
-        case 0x6b:
-        case 0x6c:
-        case 0x6d:
-        case 0x6e:
-        case 0x6f:
+        case 0x60: printf("MOV    H,B"); break;
+        case 0x61: printf("MOV    H,C"); break;
+        case 0x62: printf("MOV    H,D"); break;
+        case 0x63: printf("MOV    H,E"); break;
+        case 0x64: printf("MOV    H,H"); break;
+        case 0x65: printf("MOV    H,L"); break;
+        case 0x66: printf("MOV    H,M"); break;
+        case 0x67: printf("MOV    H,A"); break;
+        case 0x68: printf("MOV    L,B"); break;
+        case 0x69: printf("MOV    L,C"); break;
+        case 0x6a: printf("MOV    L,D"); break;
+        case 0x6b: printf("MOV    L,E"); break;
+        case 0x6c: printf("MOV    L,H"); break;
+        case 0x6d: printf("MOV    L,L"); break;
+        case 0x6e: printf("MOV    L,M"); break;
+        case 0x6f: printf("MOV    L,A"); break;
 
-        case 0x70:
-        case 0x71:
-        case 0x72:
-        case 0x73:
-        case 0x74:
-        case 0x75:
-        case 0x76:
-        case 0x77:
-        case 0x78:
-        case 0x79:
-        case 0x7a:
-        case 0x7b:
-        case 0x7c:
-        case 0x7d:
-        case 0x7e:
-        case 0x7f:
+        case 0x70: printf("MOV    M,B"); break;
+        case 0x71: printf("MOV    M,C"); break;
+        case 0x72: printf("MOV    M,D"); break;
+        case 0x73: printf("MOV    M,E"); break;
+        case 0x74: printf("MOV    M,H"); break;
+        case 0x75: printf("MOV    M,L"); break;
+        case 0x76: printf("HLT"); break;
+        case 0x77: printf("MOV    M,A"); break;
+        case 0x78: printf("MOV    A,B"); break;
+        case 0x79: printf("MOV    A,C"); break;
+        case 0x7a: printf("MOV    A,D"); break;
+        case 0x7b: printf("MOV    A,E"); break;
+        case 0x7c: printf("MOV    A,H"); break;
+        case 0x7d: printf("MOV    A,L"); break;
+        case 0x7e: printf("MOV    A,M"); break;
+        case 0x7f: printf("MOV    A,A"); break;
 
         case 0x80:
         case 0x81:
